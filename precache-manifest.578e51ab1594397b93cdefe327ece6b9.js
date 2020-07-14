be3825bb4176aeb452a259ec29a037d1b4c7549c
r0bertLiu <lyb19940313@gmail.com>
2020-07-14 18:16:25 +1000

    Updates

---
+++ precache-manifest.578e51ab1594397b93cdefe327ece6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec28eecdefe10ce97cf4509d0716955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fedf80d4cca5b31d17f4",
+    "url": "/static/css/main.f1be70ff.chunk.css"
+  },
+  {
+    "revision": "398b8042772f1b1a5bab",
+    "url": "/static/js/2.0da3285d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0da3285d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedf80d4cca5b31d17f4",
+    "url": "/static/js/main.1df05c74.chunk.js"
+  },
+  {
+    "revision": "35e5f1e8248fa932c41b",
+    "url": "/static/js/runtime-main.f788fbad.js"
+  }
+]);
